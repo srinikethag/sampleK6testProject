8f412fd1fa384883b4948892d7714da68f5f3374
Sriniketha Geejagar <sriniketha.geejagar@toggl.com>
2024-03-15 19:33:18 +0530

    added env variable loader

--- npmK6Test/loader.js
+++ npmK6Test/loader.js
@@ -1,4 +1,7 @@
 import dotenv from 'dotenv';
+import { readFileSync, writeFileSync } from 'fs';
 dotenv.config();
 
 process.env.NEW_VALUE=10
+
+fs.writeFileSync('.env', `NEW_VALUE=${process.env.NEW_VALUE}`);
