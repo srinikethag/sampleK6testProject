b845a50278eadd255e0a0c95778cd4887b9a33f9
Sriniketha Geejagar <124348587+srinikethag@users.noreply.github.com>
2024-03-15 20:42:20 +0530

    Update master.js

--- npmK6Test/master.js
+++ npmK6Test/master.js
@@ -1,17 +1,7 @@
-import { execSync } from "child_process";
 
 export function main(){
     console.log(process.env.SECRET_VALUE);
     console.log(process.env.NEW_VALUE);
-    runk6Tests();
 }
 
-function runk6Tests() {
-    try {
-        execSync('k6 run ./npmK6Test/smokeTest.js');
-    } catch (e) {
-        console.log('error ', e);
-    }
-}
-
-main()
+main();
